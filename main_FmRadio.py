95d1b2d5279ecf9c6b199e8cc177478f9c46172b
God <sanyaatanasov@gmail.com>
2024-01-23 17:08:08 +0200

    main

--- main_FmRadio.py
+++ main_FmRadio.py
@@ -2,26 +2,26 @@ from datetime import datetime as dt
 import os
 
 def get_days_from_today(date):
+    date= dt.date(date)
+    today=dt.date(dt.today())
+    print(f'Дата яку ми ввели:\n{date}\n')
+    print(f'Поточна дата:\n{today}\n')
+    print(f'Різниця кількістю днів між заданою датою і поточною датою:\n{today-date}\n')
+    
 
-    today=dt.today()
-    return
 
 def start_main_program():
     try:
-        input_time = dt(input("Ведіть дату 'РРРР-ММ-ДД'(2010,10,09): "))
-        print(input_time)
+
+        input_time=dt(year=2024, month=1, day=6)
+        #input_time = dt(year=int(input("Year= ")), month=int(input("Month= ")), day=int(input("Day= ")))
+        get_days_from_today(input_time)
+        
+
     except Exception as eror:
         os.system('cls')
         print(f"Error: {eror}")
         start_main_program()
 
-start_main_program()
-
-
-
-
-
-
-
-
-
+os.system('cls')
+start_main_program() 
