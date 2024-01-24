613754c1c9075378fe33a5da84a7568c88182712
God <sanyaatanasov@gmail.com>
2024-01-24 12:36:40 +0200

    main

--- main_FmRadio.py
+++ main_FmRadio.py
@@ -6,16 +6,16 @@ def get_days_from_today(date):
     today=dt.date(dt.today())
     print(f'Дата яку ми ввели:\n{date}\n')
     print(f'Поточна дата:\n{today}\n')
-    print(f'Різниця кількістю днів між заданою датою і поточною датою:\n{today-date}\n')
+    return today-date
     
 
 
 def start_main_program():
     try:
 
-        #input_time=dt(year=2024, month=1, day=6)
-        input_time = dt(year=int(input("Year= ")), month=int(input("Month= ")), day=int(input("Day= ")))
-        get_days_from_today(input_time)
+        input_time=dt(year=2024, month=1, day=6)
+        #input_time = dt(year=int(input("Year= ")), month=int(input("Month= ")), day=int(input("Day= ")))
+        print(f'Різниця кількістю днів між заданою датою і поточною датою:\n{get_days_from_today(input_time)}\n')
         
 
     except Exception as eror:
